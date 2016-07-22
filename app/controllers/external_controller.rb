16f125422d1fb18a22c230d32ec148244b7f2ec5
Carlos Gochez <cgotcha@gmail.com>
2016-07-21 19:58:53 -0600

    Crear el layout external y empezarlo a usar

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -1,5 +1,6 @@
 class ExternalController < ApplicationController
   skip_before_action :authenticate
+  layout 'external'
 
   def index
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate
-
+  layout 'external'
 
   def new
     if current_user.present?
